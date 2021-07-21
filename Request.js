439d64ada363bafce0c9919bf22c30062d54d907
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-21 13:24:49 -0700

    Init subrepo

---
+++ Request.js
@@ -0,0 +1,8 @@
+module.exports = class HyarcadeRequests {
+    static HypixelApi = require("./HypixelApi");
+    static hypixelReq = require("./hypixelReq");
+    static labyRequest = require("./labyRequest");
+    static mojangRequest = require("./mojangRequest");
+    static optifineRequest = require("./optifineRequest");
+    static webRequest = require("./webRequest");
+}
