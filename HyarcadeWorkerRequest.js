e6f293e38d9c81c9d630f17644fd937e79a2dc99
eatmyvenom <eat.my.venomm@gmail.com>
2021-10-25 17:09:59 -0700

    log key

--- HyarcadeWorkerRequest.js
+++ HyarcadeWorkerRequest.js
@@ -10,7 +10,7 @@ let currentKey = 0;
  * @returns {string}
  */
 function getAPIKey () {
-  logger.debug(`Using key : ${currentKey + 1} of ${keys.length}`);
+  logger.debug(`Using key : ${currentKey + 1} of ${keys.length} (${currentKey.toString().slice(0, 16)}`);
   const key = keys[currentKey];
   currentKey += 1;
   if(currentKey == keys.length) {
