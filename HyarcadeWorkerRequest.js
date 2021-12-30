0304997c2f2cd3e6fefd7e64a9c4cdaf1ace5a57
eatmyvenom <eat.my.venomm@gmail.com>
2021-12-29 21:31:30 -0800

    redo worker requests from the ground up

--- HyarcadeWorkerRequest.js
+++ HyarcadeWorkerRequest.js
@@ -1,5 +1,5 @@
-const https = require("https");
 const logger = require("hyarcade-logger");
+const webRequest = require("./webRequest");
 const cfg = require("hyarcade-config").fromJSON();
 const keys = cfg.altkeys.concat([cfg.clusters[cfg.cluster].key]);
 
@@ -28,51 +28,29 @@ class Response {
 
 /**
  * 
- * @param {string[]} accs 
+ * @param {string} uuids 
  * @returns {Promise<Response>}
  */
-async function HyarcadeWorkerRequest (accs) {
-  return new Promise((resolve, reject) => {
+async function HyarcadeWorkerRequest (uuids) {
 
-    const url = `https://hyarcade-worker.vnmm.workers.dev?pass=${cfg.dbPass}`;
+  const accs = {};
+  
+  let acc;
 
-    const reqOptions = {
-      family: 4,
-      port: 443,
-      protocol: "https:",
-      headers: {
-        apikey: getAPIKey(),
-        accs,
-      }
-    };
+  for(let i = 0;i < uuids.length;i += 1) {
+    const uuid = uuids[i];
+    acc = await webRequest(`https://api.hypixel.net/player?key=${getAPIKey()}&uuid=${uuid.trim()}`);
 
     try {
-      const requester = https.get(url, reqOptions, (res) => {
-        let reply = "";
-        res.on("data", (d) => {
-          reply += d;
-        });
-        res.on("end", () => {
-          let response;
-
-          try {
-            response = JSON.parse(reply);
-          } catch (e) {
-            reject(e);
-          }
-
-          resolve(response);
-        });
-        res.on("error", (err) => {
-          reject(err);
-        });
-      });
-
-      requester.on("error", logger.err);
+      accs[uuid.trim()] = JSON.parse(acc.data);
     } catch (e) {
-      reject(e);
+      console.log(acc.data);
+      console.log(acc.headers);
+      i -= 1;
     }
-  });
+  }
+
+  return { data: accs, key: { limit: acc.headers["ratelimit-limit"], remaining: acc.headers["ratelimit-remaining"], reset: acc.headers["ratelimit-reset"] } };
 }
 
 module.exports = HyarcadeWorkerRequest;
