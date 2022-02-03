7a3902e1736e5d9b609754730a68d1d5fd57b497
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-03 12:29:22 -0800

    feat(mongo): :card_file_box: project all stats in miw lb

--- MongoConnector.js
+++ MongoConnector.js
@@ -371,7 +371,7 @@ class MongoConnector {
             $expr: { $eq: ["$uuid", "$$uuid"] },
           },
         },
-        { $project: { [stat]: 1, _id: 0, uuid: 1 } },
+        { $project: { miniWalls: 1, _id: 0, uuid: 1 } },
       ],
       as: "historicalData",
     };
